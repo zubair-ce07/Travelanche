7592d1adfe5f34466ffcb921ac9e69e255a5ac9a
zubair-ce07 <33825083+zubair-ce07@users.noreply.github.com>
2017-11-20 13:15:48 +0500

    chawal mari h

--- application/config/config.php
+++ application/config/config.php
@@ -21,7 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
-|
+|AAAA
 */
 $config['base_url'] = 'http://localhost/travelanche/';
 
